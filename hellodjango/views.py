557db1d50ea96b1312cc705758526663bf5457c2
Bradley Voracek <brad.dv@gmail.com>
2014-03-23 12:16:17 -0700

    lol

--- hellodjango/views.py
+++ hellodjango/views.py
@@ -12,21 +12,20 @@ def get_pizzas():
   cheeseboard_soup = bs(cheeseboard_page)
   sliver_soup = bs(sliver_page)
 
-  cheeseboard_pizzas = cheeseboard_soup.findAll('p')[0:5]
-  cheeseboard_dates = cheeseboard_soup.findAll('h4')[1:6]
+  cheeseboard_pizzas = cheeseboard_soup.findAll("div",attrs={"class":"column"})
 
-  sliver_pizzas = sliver_soup.findAll('p')[0:6]
-  sliver_dates = sliver_soup.findAll('h2')[0:6]
+  sliver_pizzas = sliver_soup.findAll("div",attrs={"class":"panel menu"})
   
   html_string = "<html><body>"
 
-  for i in range(len(sliver_pizzas)):
-    html_string += str(sliver_dates[i])
-    html_string += str(sliver_pizzas[i])
-  
-  for j in range(len(cheeseboard_pizzas)):
-    html_string += str(cheeseboard_dates[j])
-    html_string += str(cheeseboard_pizzas[j])
-  
+  html_string += "<h2>SLIVER</h2>"
+
+  html_string += str(sliver_pizzas[0])[148:]
+  html_string += "<h2>CHEESEBOARD</h2>"
+  html_string += str(cheeseboard_pizzas[0])[0:1111]
+
   html_string += "</body></html>"
   return html_string
+
+if __name__ == "__main__":
+  print(get_pizzas())
