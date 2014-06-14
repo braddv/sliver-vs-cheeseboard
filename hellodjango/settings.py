10b4283c8346b838c2510f2570247087380d523c
Brad Voracek <brad.dv@gmail.com>
2014-06-13 21:39:22 -0700

    Revert "remove staticfiles dir"
    This reverts commit c20b98e19a09a932a85b7123946eb0205fa3475a.

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -83,6 +83,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 TEMPLATES_DIR = (
     '/templates/'
