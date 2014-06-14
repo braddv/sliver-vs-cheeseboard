baed1322e0f02abd2d26290efee25d8559529872
Brad Voracek <brad.dv@gmail.com>
2014-06-14 07:59:51 -0700

    dont put secret key on github

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@x1sfivix4ask_9!ol$&@qg7!69w_+$wv$j#tz3u)(8q-^s$xr'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
