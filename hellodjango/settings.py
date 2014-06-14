c20b98e19a09a932a85b7123946eb0205fa3475a
Brad Voracek <brad.dv@gmail.com>
2014-06-13 21:11:40 -0700

    remove staticfiles dir

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -83,9 +83,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 TEMPLATES_DIR = (
     '/templates/'
