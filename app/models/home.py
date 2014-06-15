8f149eaecaa1e1b77bb20192b2b9b725f39e6263
Jason <jab@tradeshift.com>
2014-06-15 15:25:02 -0700

    Cleanup python; scrape dates of pizza from sites and restructure pizza data to include it; stylize (still non-functional) mobile-only arrows intended for switching between days; TODO: use pizza date info to find if pizzas are current or not; if not, set desc to self.unknown_text & implement mobile-only day-switching arrows in javascript

--- app/models/home.py
+++ app/models/home.py
@@ -4,6 +4,12 @@ from BeautifulSoup import BeautifulSoup as bs
 from django.db import models
 
 class HomeModel(models.Model):
+    def __init__(self):
+        self.closed_text = 'Closed'
+        self.unknown_text = '???'
+        self.current_year = str(datetime.datetime.now().year)
+        self.current_day = datetime.datetime.now().strftime('%A')
+
     def get_pizzas_data(self):
         cheeseboard_html = urllib2.urlopen("http://cheeseboardcollective.coop/pizza")
         sliver_html = urllib2.urlopen("http://sliverpizzeria.com/")
@@ -15,55 +21,52 @@ class HomeModel(models.Model):
         cheeseboard_pizzas_html = parsed_cheeseboard.findAll("div",attrs={"class":"column"})
         sliver_pizzas_html = parsed_sliver.findAll("div",attrs={"class":"home-excerpt"})
 
-        cheeseboard_pizzas = [pizza.text for pizza in cheeseboard_pizzas_html[0].findAll('p')]
-        sliver_pizzas = [pizza.text for pizza in sliver_pizzas_html[0].findAll('p')]
+        cheeseboard_pizzas = [''] + [pizza.text for pizza in cheeseboard_pizzas_html[0].findAll('p')]
+        cheeseboard_pizza_dates = [pizza.text for pizza in cheeseboard_pizzas_html[0].findAll('h4')]
 
-        pizzas = [
-            {
-                'day': 'Monday',
-                'sliver': sliver_pizzas[0].strip('.'),
-                'cheeseboard': cheeseboard_pizzas[0].strip('.')
-            },
-            {
-                'day': 'Tuesday',
-                'sliver': sliver_pizzas[1].strip('.'),
-                'cheeseboard': cheeseboard_pizzas[1].strip('.')
-            },
-            {
-                'day': 'Wednesday',
-                'sliver': sliver_pizzas[2].strip('.'),
-                'cheeseboard': cheeseboard_pizzas[2].strip('.')
-            },
-            {
-                'day': 'Thursday',
-                'sliver': sliver_pizzas[3].strip('.'),
-                'cheeseboard': cheeseboard_pizzas[3].strip('.')
-            },
-            {
-                'day': 'Friday',
-                'sliver': sliver_pizzas[4].strip('.'),
-                'cheeseboard': cheeseboard_pizzas[4].strip('.')
-            },
-            {
-                'day': 'Saturday',
-                'sliver': sliver_pizzas[5].strip('.'),
-                'cheeseboard': cheeseboard_pizzas[5].strip('.')
-            },
-            {
-                'day': 'Sunday',
-                'sliver': 'Closed',
-                'cheeseboard': 'Closed'
+        sliver_pizzas = [pizza.text for pizza in sliver_pizzas_html[0].findAll('p')]
+        sliver_pizza_dates = [pizza.text for pizza in sliver_pizzas_html[0].findAll('h2')]
+
+        # TODO: Compare pizza updated date to current date
+
+        pizzas = []
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        for dayIndex in range(0, 7):
+            day = {
+                'day': days[dayIndex]
             }
-        ]
+
+            if (days[dayIndex] == 'Sunday'):
+                day['cheeseboard'] = {
+                    'desc': self.closed_text
+                }
+                day['sliver'] = {
+                    'desc': self.closed_text
+                }
+            elif (days[dayIndex] == 'Monday'):
+                day['cheeseboard'] = {
+                    'desc': self.closed_text
+                }
+                day['sliver'] = {
+                    'desc': sliver_pizzas[dayIndex].strip('.'),
+                    'date': sliver_pizza_dates[dayIndex].split(' ')[1]
+                }
+            else:
+                day['cheeseboard'] = {
+                    'desc': cheeseboard_pizzas[dayIndex].strip('.'),
+                    'date': cheeseboard_pizza_dates[dayIndex].split(' ')[1]
+                }
+                day['sliver'] = {
+                    'desc': sliver_pizzas[dayIndex].strip('.'),
+                    'date': sliver_pizza_dates[dayIndex].split(' ')[1]
+                }
+
+            pizzas.append(day)
 
         reorderedPizzas = []
-
-        currentDay = datetime.datetime.now().strftime('%A')
-
-
         startingIndex = -1
         for index, pizza in enumerate(pizzas):
-            if (pizza['day'] == currentDay):
+            if (pizza['day'] == self.current_day):
                 startingIndex = index
             if (startingIndex > -1):
                 reorderedPizzas.append(pizza)
@@ -73,6 +76,5 @@ class HomeModel(models.Model):
                 reorderedPizzas.append(pizza)
             else:
                 break
-
 
         return reorderedPizzas
